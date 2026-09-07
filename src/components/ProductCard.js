aa997f6cdbb5dd57f44b2c9de3408dd1094e758d
DarkLightningProject <ripudamansinghmultani@gmail.com>
2026-09-07 11:38:27 +0530

    v3

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,7 +1,6 @@
 import "./ProductCard.css";
 import { useEffect, useState } from "react";
 import { useCart } from "../context/CartContext";
-import { removeFromCart } from "../context/CartContext";
 
 export const ProductCard = ({ product }) => {
   const { addToCart, cartList, removeFromCart } = useCart();
